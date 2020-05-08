38d67cf620efe7865b31adbda4f9991965c21255
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-05-08 12:13:44 +0200

    Add event property and two way bindings

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -2,14 +2,27 @@
   <div>
     <h1>Create an Event</h1>
     <form>
-      <input type="text" name="title" placeholder="Add a Title" />
+      <input
+        v-model="event.title"
+        type="text"
+        name="title"
+        placeholder="Add a Title"
+      />
       <input type="submit" value="create" />
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      event: {
+        title: ""
+      }
+    };
+  }
+};
 </script>
 
 <style scoped></style>
--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -20,7 +20,7 @@ describe("CreateEvent", () => {
   test("it should contain a title input field", () => {
     const wrapper = mount(CreateEvent);
 
-    expect(wrapper.contains("input[type='title'][type='text']")).toBe(true);
+    expect(wrapper.contains("input[name='title'][type='text']")).toBe(true);
   });
   test("it should contain submit input field", () => {
     const wrapper = mount(CreateEvent);
@@ -33,5 +33,20 @@ describe("CreateEvent", () => {
     const titleInput = wrapper.get("input[name='title']");
 
     expect(titleInput.attributes("placeholder")).toBe("Add a Title");
+  });
+
+  test("it should have an event data property", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.vm.event).toEqual({ title: "" });
+  });
+  test("it should bind the event title to the user input", () => {
+    const wrapper = mount(CreateEvent);
+
+    const titleInput = wrapper.get("input[name='title']");
+
+    titleInput.setValue("TEST_INPUT");
+
+    expect(wrapper.vm.event.title).toBe("TEST_INPUT");
   });
 });
