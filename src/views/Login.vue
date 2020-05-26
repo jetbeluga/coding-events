e79e34477e257974c8d49d6d122438ca8d76d0b4
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-05-26 15:18:23 +0200

    Implement Login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>Login</div>
+  <div>
+    <form @submit.prevent="submit">
+      <input
+        v-model="user.email"
+        type="email"
+        name="email"
+        placeholder="Add a email"
+      />
+      <input
+        v-model="user.password"
+        type="password"
+        name="password"
+        placeholder="password"
+      />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    async submit() {
+      try {
+        // Dispatch the login action
+        await this.$store.dispatch("login", this.user);
+        this.$router.push("/");
+      } catch {
+        this.$store.dispatch("pushNotification", {
+          type: "error",
+          message: "Unable to login, please check your input again",
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped></style>
